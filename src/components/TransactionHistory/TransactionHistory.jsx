a88f3076ecaf2ac7b555a0906f6f609d28b6156d
Hubenko Oksana <17zov89@gmail.com>
2021-01-15 11:31:08 +0200

    изменения структуры расположения файлов

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TransactionHistoryItem from './TransactionHistoryItem'
+import s from './transaction.module.css'
+function TransactionHistory({ type, amount, currency, id, items, item }) {
+  return (
+    <table className={s.transactionHistory}>
+  <thead>      
+        <tr>
+        <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>          
+      </thead>
+  <tbody>
+  {items.map(item=>(
+<TransactionHistoryItem 
+ key={item.id}
+ type={item.type}
+ amount={item.amount}
+ currency={item.currency}
+ />
+  ))}
+</tbody>
+</table>
+    )
+};
+
+TransactionHistory.propTypes={
+    friends:PropTypes.arrayOf(
+        PropTypes.shape({
+            id:PropTypes.number.isRequired,
+        })
+    ),
+};
+
+export default TransactionHistory;
